6539c179f645eb719318f579f4e2d73141c33f15
Houwaida Hidri <houwaidahidri@gmail.com>
2025-07-01 20:11:43 +0100

    Update SpeechToLight.py

--- Speech-to-Light/Code/SpeechToLight.py
+++ Speech-to-Light/Code/SpeechToLight.py
@@ -6,4 +6,8 @@ GPIO.setmode(GPIO.BCM)
 LED_PIN=17
 GPIO.setup(LED_PIN,GPIO.OUT)
 
-
+r=sr.Recognizer()
+with sr.Microphone() as source : 
+  print("Say Something ")
+  audio=r.listen(source)
+  voice_data=r.recognize_google(audio) 
